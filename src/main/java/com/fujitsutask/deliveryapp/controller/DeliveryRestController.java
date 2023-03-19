cec6922d7e5bdfbd7eb5a055b14776a0491eaac7
alllip <alllip@ttu.ee>
2023-03-19 18:34:23 +0200

    Created a basic REST controller for managing endpoints.

---
+++ src/main/java/com/fujitsutask/deliveryapp/controller/DeliveryRestController.java
@@ -0,0 +1,27 @@
+package com.fujitsutask.deliveryapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Main controller class for handling incoming REST interface requests.
+ */
+@RestController
+public class DeliveryRestController {
+
+    /**
+     * Endpoint for requesting the delivery fee.
+     * <p/>
+     * The delivery fee is calculated based on the given {@code cityId} and {@code vehicleId} path variables.
+     * @param cityId The ID that represents the city for which the delivery fee is calculated.
+     * @param vehicleId ID of the vehicle used in the delivery.
+     * @return The delivery fee for the requested city using the requested vehicle.
+     */
+    @GetMapping("/cities/{city_id}/vehicles/{vehicle_id}")
+    public String getDeliveryFee(@PathVariable("city_id") Integer cityId,
+                                 @PathVariable("vehicle_id") Integer vehicleId) {
+        return String.format("You made request to\nCityID: %s,\nVehicleID: %s", cityId, vehicleId);
+    }
+
+}
