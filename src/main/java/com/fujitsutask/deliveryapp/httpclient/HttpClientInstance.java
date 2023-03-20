1665e9ea386fb06d1e5bc45e5e3a20bc8f818090
alllip <alllip@ttu.ee>
2023-03-20 20:58:53 +0200

    Added docstings.

--- src/main/java/com/fujitsutask/deliveryapp/httpclient/HttpClientInstance.java
+++ src/main/java/com/fujitsutask/deliveryapp/httpclient/HttpClientInstance.java
@@ -2,9 +2,18 @@ package com.fujitsutask.deliveryapp.httpclient;
 
 import okhttp3.OkHttpClient;
 
+/**
+ * Singleton class used to retrieve the object that can be used to create HTTP requests.
+ * <p/>
+ * The class instance can be gotten through the {@code HttpClientInstance.getInstance()} method.
+ */
 public class HttpClientInstance {
     private static OkHttpClient instance = null;
 
+    /**
+     * Returns the instance used to make HTTP requests.
+     * @return Instance of class used for HTTP communication.
+     */
     public static OkHttpClient getInstance() {
         if (instance == null) {
             instance = new OkHttpClient();
